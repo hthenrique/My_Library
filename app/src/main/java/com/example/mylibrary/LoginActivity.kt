54331477a404810258acc59a373d107e6b6ef4ba
Henrique Teixeira <ht.henrique@live.com>
2023-04-08 16:01:52 -0300

    login and return response

--- app/src/main/java/com/example/mylibrary/LoginActivity.kt
+++ app/src/main/java/com/example/mylibrary/LoginActivity.kt
@@ -5,32 +5,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mylibrary.databinding.ActivityMainBinding
+import com.example.mylibrary.model.LoginRequest
 import com.example.mylibrary.model.LoginResponse
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var loginRequest: LoginRequest
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        val email = binding.editTextTextPersonName.toString()
+        val password = binding.editTextTextPersonPassword.toString()
+        loginRequest = LoginRequest(email, password)
 
-
+        binding.loginButton.setOnClickListener {
+            returnLoginResult()
+        }
     }
 
     private fun returnLoginResult(){
         val loginResponse = LoginResponse(
             retCode = 2000,
-            message = "Success")
+            message = "Success with:" + loginRequest.email)
 
         val intent: Intent = Intent()
         intent.putExtra("loginResponse", loginResponse)
 
         setResult(Activity.RESULT_OK, intent)
 
-        finish()
+        this.finish()
     }
 }
 
---
+++ app/src/main/java/com/example/mylibrary/model/LoginRequest.kt
@@ -0,0 +1,4 @@
+package com.example.mylibrary.model
+
+class LoginRequest(val email:String, val password:String) {
+}
